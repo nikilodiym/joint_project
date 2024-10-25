e5604f9e928baaa8ceec433e45bcae29cb902077
Nikita Krenov <nikitabaram@gmail.com>
2024-10-25 21:10:56 +0300

    Nikita work in to file in translator.cpp and translator.h

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
+#include "translator.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 int main() {
-	
-
-	system("pause");
-	return 0;
+ 
+    system("pause");
+    return 0;
 }
--- translator.h
+++ translator.h
@@ -1,11 +1,9 @@
-#pragma once
 #ifndef TRANSLATOR_H
 #define TRANSLATOR_H
 
 #include <string>
-
 using namespace std;
 
-void translate(const string& target_lenguage, const string& word);
+void translate(const string& targetLanguage, const string& text);
 
-#endif // !TRANSLATOR_H
+#endif

3f662868aa51cdd1bd2df6c9e12ae0d8c0d42037
kakynchik <ukakyn@gmail.com>
2024-10-26 09:27:32 +0300

    to sho nada

--- main.cpp
+++ main.cpp
@@ -5,7 +5,15 @@
 using namespace std;
 
 int main() {
- 
+    string targetLanguage, text;
+
+    cout << "Enter the target language: ";
+    getline(cin, targetLanguage);
+    cout << "Enter text in English to translate: ";
+    getline(cin, text);
+
+    translate(targetLanguage, text);
+
     system("pause");
     return 0;
 }
